3ed6327a89adcc3a53c673ee5ca1ed883767d2e4
M0destin <modzius29@gmail.com>
2025-02-02 16:07:32 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
-var Fuz = "Fuzz";
-var Buz = "Buzz";
-var FuzBuz = "FuzzBuzz";
-for(let i = 0; i <= 100; i++){
-    let output = "";
+function whosPaying() {
     
-    if(i % 3 === 0 && i % 5 === 0){
-        output = FuzBuz;
-    }
-    else if(i % 3 === 0){
-        output = Fuz;
-    }
-    else if(i % 5 === 0){
-        output = Buz;
-    }
-    else{
-        output = i;
-    }
-    console.log(output);
+    var arrayOfNames = ["Angela", "Ben", "Jenny", "Michael", "Chloe"];
+    
+    var mathcalc = Math.round(Math.random() * (arrayOfNames.length - 1));
+
+    var payer = arrayOfNames[mathcalc];
+    
+    return payer + " is going to buy lunch today";
+  
 }
+
