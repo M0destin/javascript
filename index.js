4fa4d78424433e23682cf553f383be3dc0977666
Ce0o <modzius29@gmail.com>
2025-02-17 21:10:56 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-function whosPaying() {
-    
-    var arrayOfNames = ["Angela", "Ben", "Jenny", "Michael", "Chloe"];
-    
-    var mathcalc = Math.round(Math.random() * (arrayOfNames.length - 1));
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
 
-    var payer = arrayOfNames[mathcalc];
-    
-    return payer + " is going to buy lunch today";
-  
-}
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
 
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+var port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) =>{
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", (req, res) => {
+    var storage = req.body["password"];
+    if(storage === "Hello"){
+        res.sendFile(__dirname + "/public/secret.html");
+    }
+    else{
+      res.sendFile(__dirname + "/public/index.html");
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Running on port: ${port}`);
+});
